8d225daeecbf7ff3af16ff7860dabc40d0a3a111
Rodrigo Emanuel <drigosantos@gmail.com>
2019-10-27 10:57:12 -0300

    Bispo

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import boardgame.Peca;
 import boardgame.Posicao;
 import boardgame.Tabuleiro;
+import xadrez.pecas.Bishop;
 import xadrez.pecas.King;
 import xadrez.pecas.Pawn;
 import xadrez.pecas.Rook;
@@ -193,7 +194,9 @@ public class PartidaXadrez {
 	
 	private void initialSetup() {
 		lugarNovaPeca('a', 1, new Rook(tabuleiro, Cor.WHITE));
+		lugarNovaPeca('c', 1, new Bishop(tabuleiro, Cor.WHITE));
 		lugarNovaPeca('e', 1, new King(tabuleiro, Cor.WHITE));
+		lugarNovaPeca('f', 1, new Bishop(tabuleiro, Cor.WHITE));
 		lugarNovaPeca('h', 1, new Rook(tabuleiro, Cor.WHITE));
 		lugarNovaPeca('a', 2, new Pawn(tabuleiro, Cor.WHITE));
 		lugarNovaPeca('b', 2, new Pawn(tabuleiro, Cor.WHITE));
@@ -205,7 +208,9 @@ public class PartidaXadrez {
 		lugarNovaPeca('h', 2, new Pawn(tabuleiro, Cor.WHITE));
 
 		lugarNovaPeca('a', 8, new Rook(tabuleiro, Cor.BLACK));
+		lugarNovaPeca('c', 8, new Bishop(tabuleiro, Cor.BLACK));
 		lugarNovaPeca('e', 8, new King(tabuleiro, Cor.BLACK));
+		lugarNovaPeca('f', 8, new Bishop(tabuleiro, Cor.BLACK));
 		lugarNovaPeca('h', 8, new Rook(tabuleiro, Cor.BLACK));
 		lugarNovaPeca('a', 7, new Pawn(tabuleiro, Cor.BLACK));
 		lugarNovaPeca('b', 7, new Pawn(tabuleiro, Cor.BLACK));
---
+++ src/xadrez/pecas/Bishop.java
@@ -0,0 +1,68 @@
+package xadrez.pecas;
+
+import boardgame.Posicao;
+import boardgame.Tabuleiro;
+import xadrez.Cor;
+import xadrez.PecaXadrez;
+
+public class Bishop extends PecaXadrez {
+
+	public Bishop(Tabuleiro tabuleiro, Cor cor) {
+		super(tabuleiro, cor);
+	}
+
+	@Override
+	public String toString() {
+		return "B";
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+
+		Posicao p = new Posicao(0, 0);
+
+		// noroeste
+		p.setValores(posicao.getLinha() - 1, posicao.getColuna() - 1);
+		while (getTabuleiro().posicaoExiste(p) && !getTabuleiro().temUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValores(p.getLinha() - 1, p.getColuna() -1);
+		}
+		if (getTabuleiro().posicaoExiste(p) && temPecaOponente(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// nordeste
+		p.setValores(posicao.getLinha() - 1, posicao.getColuna() + 1);
+		while (getTabuleiro().posicaoExiste(p) && !getTabuleiro().temUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValores(p.getLinha() - 1, p.getColuna() + 1);
+		}
+		if (getTabuleiro().posicaoExiste(p) && temPecaOponente(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// sudeste
+		p.setValores(posicao.getLinha() + 1, posicao.getColuna() + 1);
+		while (getTabuleiro().posicaoExiste(p) && !getTabuleiro().temUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValores(p.getLinha() + 1, p.getColuna() + 1);
+		}
+		if (getTabuleiro().posicaoExiste(p) && temPecaOponente(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// sudoeste
+		p.setValores(posicao.getLinha() + 1, posicao.getColuna() - 1);
+		while (getTabuleiro().posicaoExiste(p) && !getTabuleiro().temUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValores(p.getLinha() + 1, p.getColuna() - 1);
+		}
+		if (getTabuleiro().posicaoExiste(p) && temPecaOponente(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		return mat;
+	}
+
+}
